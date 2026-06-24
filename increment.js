29f5dd9c57fb10742375b95111d5ecb7589f8657
EVANS-OSASERE <evansdahkvng@gmail.com>
2026-06-24 18:43:34 +0100

    my increment

---
+++ increment.js
@@ -0,0 +1,11 @@
+let John = document.getElementById("value");
+
+function plus_increase(){
+	John.innerHTML++;
+	John.innerHTML=John.innerHTML;
+}
+
+function minus_decrease(){
+	John.innerHTML--;
+	John.innerHTML=John.innerHTML
+}
